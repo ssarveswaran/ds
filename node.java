285cd7cb7e26cc3c43e45cbd34e9c31286d0785e
sarveswaran <106679925+ssarveswaran@users.noreply.github.com>
2023-10-07 21:06:57 +0530

    Update node.java

--- node.java
+++ node.java
@@ -4,21 +4,4 @@ public class node {
     
     
 }
-function equals() {
-    var txt = document.form.textview.value;
-    try {
-        if (txt) {
-            var result = eval(txt);
-            if (isNaN(result) || !isFinite(result)) {
-                document.form.textview.value = 'ERROR';
-            } else {
-                document.form.textview.value = result;
-            }
-        } else {
-            document.form.textview.value = 'ERROR';
-        }
-    } catch (error) {
-        document.form.textview.value = 'ERROR';
-    }
-}
-https://ahmedsaheel.github.io/Ahmed_portfolio/
+
